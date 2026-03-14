c3f121ac31945f5c8a4ebad060ffd952e694b55a
michael chen <mchaelchnn@gmail.com>
2026-03-14 15:47:38 -0400

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,11 +25,9 @@ export const metadata: Metadata = {
   applicationName: "Hello",
   manifest: "/manifest.webmanifest",
   icons: {
-    icon: [
-      { url: "/icon-192.svg", sizes: "192x192", type: "image/svg+xml" },
-      { url: "/icon-512.svg", sizes: "512x512", type: "image/svg+xml" },
-    ],
-    apple: [{ url: "/apple-touch-icon.svg", sizes: "180x180" }],
+    icon: [{ url: "/logo%202.png", type: "image/png" }],
+    shortcut: [{ url: "/logo%202.png", type: "image/png" }],
+    apple: [{ url: "/logo%202.png", type: "image/png" }],
   },
   appleWebApp: {
     capable: true,
